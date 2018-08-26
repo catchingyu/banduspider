ad9ecdae449bab38d3719d1ebadb65d4cfbfc73b
catchingyu <catchingyu@163.com>
2018-08-26 21:32:39 +0800

    update get the audio but the player is not loader automatically

---
+++ getaudiofiles.py
@@ -0,0 +1,90 @@
+# !/usr/bin/python 
+# -*- coding: utf-8 -*-
+# -*- coding: gdb -*-
+
+import time
+import datetime
+import glob
+import urllib2
+import json
+import sys
+import re
+import sqlite3
+import random
+import threading
+import os
+
+#https://res.wx.qq.com/voice/getvoice?mediaid=MzA5MDE3MTE1NF8yMjQ3NDg0NzEy
+def get_audiofile(itemURL):
+	pre_url = 'https://res.wx.qq.com/voice/getvoice?mediaid='
+	try:
+		print 'Downloading itemURL'+ itemURL + '\n'
+		response = urllib2.urlopen(itemURL)
+		response = urllib2.urlopen(itemURL)
+	except urllib2.HTTPError, e:
+		print e
+		return ''
+	except StandardError, e:
+		print e
+		return ''
+	json_page_value = response.read().decode('utf-8')
+	print json_page_value
+	#<span id="voice_play_MzA5MDE3MTE1NF8yMjQ3NDkwMjMw_0" aria-labelledby="播放开关" class="db share_audio_switch"><em class="icon_share_audio_switch" role="button"></em></span>
+	#https://res.wx.qq.com/voice/getvoice?mediaid=MzA5MDE3MTE1NF8yMjQ3NDkwMjMw
+	item_re = re.compile(
+		r'voice_encode_fileid="(.*?)">',
+		re.I)
+	match = item_re.match(json_page_value)
+	if match:
+		entry = match.groups()
+		print entry[0], entry[2]
+	return
+
+#get pageinfo and return the name and URL
+#http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490366&amp;idx=6&amp;sn=1f7c685c0d943b92a28239cecb375c4a&amp;chksm=900ee943a7796055c8e3900da847c03edf90fe6c17e82f096aa7f16fbed4e901092b788f9c20&amp;scene=21#wechat_redirect
+#  【单读】许知远：开罗之声
+#http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect
+def process_pageinfo(pagehtml):
+	tr_re = re.compile(r'<p>(.*?)</p>')
+	item_re = re.compile(
+		r'<a href="(.*?)"  target(.*?)>(.*?)</a>',
+		re.I)
+	for line in tr_re.findall(pagehtml):
+		#print line + '\n'
+		match = item_re.match(line)
+		if match:
+			entry = match.groups()
+			print entry[0], entry[2]
+
+def get_audiolistURL(albumURL):
+	try:
+		print 'Downloading albumURL'+ albumURL + '\n'
+		response = urllib2.urlopen(albumURL)
+	except urllib2.HTTPError, e:
+		print e
+		return ''
+	except StandardError, e:
+		print e
+		return ''
+	json_page_value = response.read().decode('utf-8')
+	#print json_page_value
+	return json_page_value
+
+
+
+def main(argv):
+#	html = get_audiolistURL("http://www.bandubb.com/dandu/27732.html")
+	itemURL = 'http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect'
+	itemURL = 'http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21'
+#	process_pageinfo(html)
+	get_audiofile(itemURL)
+	sys.exit(0)
+
+if __name__ == "__main__":
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+	main(sys.argv)
+
+
+
+
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import urllib2
 import sys
 import json
 import glob
+import re
 
 fund_info_list = ['Date', 'NetValue', 'AccuValue', 'DayIncrease', 'Bonus']
 
@@ -23,10 +24,6 @@ def getEveryDay(begin_date, end_date):
 def test2():
 	# 获取两个日期间的所有日期
 	print(getEveryDay('2017-01-01', '2017-01-11'))
-
-
-
-
 def test1():
 	bonusDay = "每份派现金0.0097元"
 	# process the bonustofloat
@@ -34,8 +31,6 @@ def test1():
 	print bonusDay
 	bonusDay=float(bonusDay)
 	print bonusDay*50
-
-
 def gen_fund_insert_command(tablename, info_dict):
 	"""
 	fund insert command
@@ -52,14 +47,38 @@ def gen_fund_insert_command(tablename, info_dict):
 	print command
 	return command
 
+def test3():
+	pagehtml = '<a href="http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect"  target="_blank" rel="noopener noreferrer">【单读】许知远：春之祭+日瓦戈医生</a>'
+	tr_re = re.compile(r'<p>(.*?)</p>')
+	item_re = re.compile(
+		r'<a href="(.*?)">(.*?)</a>',
+		re.X)
+	for line in tr_re.findall(pagehtml):
+		print line + '\n'
+		match = item_re.match(line)
+		if match:
+			entry = match.groups()
+			print entry
+
+def test4():
+	line = '<a href="http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect"  target="_blank" rel="noopener noreferrer">【单读】许知远：春之祭+日瓦戈医生</a>'
+	item_re = re.compile(
+		r'<a href="(.*?);(.*?)">(.*?)</a>',
+		re.I)
+	match = item_re.match(line)
+	if match:
+		entry = match.groups()
+		print entry[0], entry[2]
+
 if __name__=="__main__":
 	print "start..."
-	info_dict = {}
-	info_dict.update({fund_info_list[0]: 1})
-	info_dict.update({fund_info_list[1]: 2})
-	info_dict.update({fund_info_list[2]: 3})
-	info_dict.update({fund_info_list[3]: 4})
-	info_dict.update({fund_info_list[4]: 5})
+#	test4()
+#	info_dict = {}
+#	info_dict.update({fund_info_list[0]: 1})
+#	info_dict.update({fund_info_list[1]: 2})
+#	info_dict.update({fund_info_list[2]: 3})
+#	info_dict.update({fund_info_list[3]: 4})
+#	info_dict.update({fund_info_list[4]: 5})
 	#gen_fund_insert_command("10000", info_dict)
-	test2()
+#	test2()
 	sys.exit(0)
