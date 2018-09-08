9cb26b7184fe9528f1eeb08714214e450c5697dd
catchingyu <catchingyu@163.com>
2018-09-08 23:15:15 +0800

    parse the right audio path

--- getaudiofiles.py
+++ getaudiofiles.py
@@ -14,12 +14,16 @@ import random
 import threading
 import os
 
+
+def download_audiofile_fromURL(mediaURL):
+
+	return
 #https://res.wx.qq.com/voice/getvoice?mediaid=MzA5MDE3MTE1NF8yMjQ3NDg0NzEy
 def get_audiofile(itemURL):
 	pre_url = 'https://res.wx.qq.com/voice/getvoice?mediaid='
 	try:
 		print 'Downloading itemURL'+ itemURL + '\n'
-		response = urllib2.urlopen(itemURL)
+#		response = urllib2.urlopen(itemURL)
 		response = urllib2.urlopen(itemURL)
 	except urllib2.HTTPError, e:
 		print e
@@ -28,16 +32,20 @@ def get_audiofile(itemURL):
 		print e
 		return ''
 	json_page_value = response.read().decode('utf-8')
-	print json_page_value
+	#print json_page_value
 	#<span id="voice_play_MzA5MDE3MTE1NF8yMjQ3NDkwMjMw_0" aria-labelledby="播放开关" class="db share_audio_switch"><em class="icon_share_audio_switch" role="button"></em></span>
 	#https://res.wx.qq.com/voice/getvoice?mediaid=MzA5MDE3MTE1NF8yMjQ3NDkwMjMw
+	tr_re = re.compile(r'<p>(.*?)</p>')
 	item_re = re.compile(
-		r'voice_encode_fileid="(.*?)">',
-		re.I)
-	match = item_re.match(json_page_value)
-	if match:
-		entry = match.groups()
-		print entry[0], entry[2]
+		r'voice_encode_fileid="(.*?)">')
+	for line in tr_re.findall(json_page_value):
+		#print line + '\n'
+		match = item_re.search(line)
+		#print match
+		if match:
+			entry = match.groups()
+			print entry[0]
+			download_audiofile_fromURL(pre_url + entry[0])
 	return
 
 #get pageinfo and return the name and URL
@@ -74,7 +82,7 @@ def get_audiolistURL(albumURL):
 
 def main(argv):
 #	html = get_audiolistURL("http://www.bandubb.com/dandu/27732.html")
-	itemURL = 'http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect'
+#	itemURL = 'http://www.bandubb.com/wp-content/themes/begin/inc/go.php?url=http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21#wechat_redirect'
 	itemURL = 'http://mp.weixin.qq.com/s?__biz=MzA5MDE3MTE1NA==&amp;mid=2247490232&amp;idx=6&amp;sn=7f64064a05329d6d1dd600f20426e444&amp;chksm=900ee8c5a77961d38f5da85543fc4913c49471e047013bffd00184bccf7ae43e61c70c4a31b0&amp;scene=21'
 #	process_pageinfo(html)
 	get_audiofile(itemURL)
